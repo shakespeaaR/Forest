a7752bd88f8e8c766f546ff64b5b7e85ab6f1f24
Bartosz S. <bartosz_szymanski@outlook.com>
2024-01-22 22:12:21 +0100

    added footer, RWD for mobiledevices and offers page

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 const burgerBtn = document.querySelector('.mobilemenu-icon')
 const mobileMenu = document.querySelector('.mobilemenu')
+const footerYear = document.querySelector('.year')
 
 burgerBtn.addEventListener('click', () => {
 	mobileMenu.classList.toggle('mobilemenu-active')
 	burgerBtn.classList.toggle('mobilemenu-icon__close')
 })
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.textContent = year
+}
+
+handleCurrentYear()
