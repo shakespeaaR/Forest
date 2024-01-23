6b576bd8d85418c81e6e6c1ad0558649b8959a80
Bartosz S. <bartosz_szymanski@outlook.com>
2024-01-23 23:14:31 +0100

    added scrollspy, contact page with form and google map

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,19 @@
 const burgerBtn = document.querySelector('.mobilemenu-icon')
 const mobileMenu = document.querySelector('.mobilemenu')
 const footerYear = document.querySelector('.year')
+const navMenuLink = document.querySelectorAll('.nav-items__itemlink')
 
-burgerBtn.addEventListener('click', () => {
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+	target: '#navbar-example',
+})
+
+const openCloseMenu = () => {
 	mobileMenu.classList.toggle('mobilemenu-active')
 	burgerBtn.classList.toggle('mobilemenu-icon__close')
+}
+
+navMenuLink.forEach(link => {
+	link.addEventListener('click', openCloseMenu)
 })
 
 const handleCurrentYear = () => {
@@ -12,4 +21,5 @@ const handleCurrentYear = () => {
 	footerYear.textContent = year
 }
 
+burgerBtn.addEventListener('click', openCloseMenu)
 handleCurrentYear()
